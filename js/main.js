5e90308703ee1d315e8243732dd6126243d77c8f
Vashthestupid <loic.tauveron62@gmail.com>
2020-02-14 21:07:17 +0100

    Création des boutons du formulaire"

--- js/main.js
+++ js/main.js
@@ -27,7 +27,9 @@ $(document).ready(function(){
        });
 
     
-       // Changement des thèmes
+       // Changement au niveau des thèmes
+
+/*Création du thème Playstation*/
 
        $('#playstation').click(function(){
               $('body').css('background', '#093d91');
@@ -38,12 +40,17 @@ $(document).ready(function(){
               $('.barre').css('color', 'white');
               $('label').css('color', '#fff');
               $('input').css('color', '#000');
-              $('.valid').replaceWith("<button class='btnPsX' type='submit'>X</button>");
-              $('.reset').replaceWith("<button class=''btnPsO' type='reset'>O</button>");
-              $('.btnPsX').css(
-                     'background-color', '#555555'
-                     );    
+              $('.valid').replaceWith('<button class="btnPsX" type="submit" ">X</button>');
+              $('.reset').replaceWith('<button class="btnPsO" type="reset">O</button>');
+              $('.btnXboxA').replaceWith('<button type="submit" class="btnPsX">X</button>');
+              $('.btnXboxB').replaceWith('<button class="btnPsO" type="reset">O</button>');
+              $('.btnPsX').css('color', '#80a5d1',);
+              $('.btnPsX').css('font-size','51px');
+              $('.btnPsO').css('color', '#d9a0a7');
+              $('.btnPsO').css('font-size', '51px');
        });
+
+/*Création du thème Xbox*/
 
        $('#xbox').click(function(){
               $('body').css('background', '#44bd32');
@@ -51,8 +58,11 @@ $(document).ready(function(){
               $('h2').css('color', '#fff');
               $('.marque').css('color', '#1a7b10');
               $('.titre').css('color', 'black');
-              $('.valid').replaceWith("<button class='btnXboxA'>A</button>")
-              $('.reset').replaceWith("<button class='btnXboxB'>B</button>")
+              $('.valid').replaceWith("<button class='btnXboxA' type='submit'>A</button>")
+              $('.reset').replaceWith("<button class='btnXboxB' type='reset'>B</button>")
+              $('.btnPsX').replaceWith("<button class='btnXboxA' type='submit'>A</button>")
+              $('.btnPsO').replaceWith("<button class='btnXboxB' type='reset'>B</button>")
+              $('.btnXboxA').css('color','#dceec4');
               $('.btnXboxA').css('background-color', '#8bc24a');
               $('.btnXboxA').css('box-shadow','0px 9px #689f37');
               $('.btnXboxA').mouseenter(function(){
@@ -60,15 +70,55 @@ $(document).ready(function(){
                             'box-shadow','0px 6px #689f37',
                             'transform','translateY(3px)'
                      );
-              })
+              });
               $('.btnXboxA').mouseleave(function(){
                      $(this).css(
-                            'box-shadow', '0px 9px #689f37',
+                            'box-shadow','0px 9px #689f37',
                             'transform','none'
                      );
-              })
+              });
+              $('.btnXboxA').mousedown(function(){
+                     $(this).css(
+                            'box-shadow','0px 3px #689f37',
+                            'transform','translateY(7px)'
+                     );
+              });
+              $('.btnXboxA').mouseup(function(){
+                     $(this).css(
+                            'box-shadow','0px 6px #689f37',
+                            'transform','translateY(3px)'
+                     );
+              });
+              $('.btnXboxB').css('color', '#ffc9b4');
+              $('.btnXboxB').css('background-color', '#fb5722');
+              $('.btnXboxB').css('box-shadow','0px 9px #dd2c00');
+              $('.btnXboxB').mouseenter(function(){
+                     $(this).css(
+                            'box-shadow','0px 6px #dd2c00',
+                            'transform','translateY(3px)'
+                     );
+              });
+              $('.btnXboxB').mouseleave(function(){
+                     $(this).css(
+                            'box-shadow','0px 9px #dd2c00',
+                            'transform','none'
+                     );
+              });
+              $('.btnXboxB').mousedown(function(){
+                     $(this).css(
+                            'box-shadow','0px 3px #dd2c00',
+                            'transform','translateY(7px)'
+                     );
+              });
+              $('.btnXboxB').mouseup(function(){
+                     $(this).css(
+                            'box-shadow','0px 6px #dd2c00',
+                            'transform','translateY(3px)'
+                     );
+              });
        });
 
+/*Création du thème Nintendo*/
 
        $('#nintendo').click(function(){
               $('body').css('background', '#fefee2');
@@ -76,6 +126,10 @@ $(document).ready(function(){
               $('h2').css('color', '#000');
               $('.marque').css('color', '#000');
               $('.titre').css('color', 'black');
+              $('.btnXboxA').replaceWith('<button type="submit" class="valid">A</button>');
+              $('.btnXboxB').replaceWith('<button class="reset" type="reset">B</button>');
+              $('.btnPsX').replaceWith("<button class='valid' type='submit'>A</button>")
+              $('.btnPsO').replaceWith("<button class='reset' type='reset'>B</button>")
        });
        
 });
