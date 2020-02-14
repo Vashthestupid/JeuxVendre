168e86a51dc3935de4ea3d9695559f597db87bd7
Vashthestupid <loic.tauveron62@gmail.com>
2020-02-14 11:49:09 +0100

    création ds boutons pour thèmes

--- js/main.js
+++ js/main.js
@@ -36,24 +36,37 @@ $(document).ready(function(){
               $('.marque').css('color', '#000');
               $('.titre').css('color', 'white');
               $('.barre').css('color', 'white');
-              $('.formulaire').css('background-color', '#000');
               $('label').css('color', '#fff');
-              $('input').css('color', '#fff');
-              $('.valid').replaceWith("<button class='btnPs' type='submit'>A</button>");
-              $('.reset').replaceWith("<button class=''btnXbox' type='reset'>B</button>");
-              $('.btnPs').css(
-                     'background-color', '#8bc24a'
-                     );
-             
-              
+              $('input').css('color', '#000');
+              $('.valid').replaceWith("<button class='btnPsX' type='submit'>X</button>");
+              $('.reset').replaceWith("<button class=''btnPsO' type='reset'>O</button>");
+              $('.btnPsX').css(
+                     'background-color', '#555555'
+                     );    
        });
 
        $('#xbox').click(function(){
               $('body').css('background', '#44bd32');
               $('#menu').css('background', '#2f3640');
               $('h2').css('color', '#fff');
-              $('.marque').css('color', '#1a7b10')
-              $('.titre').css('color', 'black')
+              $('.marque').css('color', '#1a7b10');
+              $('.titre').css('color', 'black');
+              $('.valid').replaceWith("<button class='btnXboxA'>A</button>")
+              $('.reset').replaceWith("<button class='btnXboxB'>B</button>")
+              $('.btnXboxA').css('background-color', '#8bc24a');
+              $('.btnXboxA').css('box-shadow','0px 9px #689f37');
+              $('.btnXboxA').mouseenter(function(){
+                     $(this).css(
+                            'box-shadow','0px 6px #689f37',
+                            'transform','translateY(3px)'
+                     );
+              })
+              $('.btnXboxA').mouseleave(function(){
+                     $(this).css(
+                            'box-shadow', '0px 9px #689f37',
+                            'transform','none'
+                     );
+              })
        });
 
 
